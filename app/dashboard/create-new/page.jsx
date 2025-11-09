9105d846e89e3befeda914a16f19623d2134601f
anantvm <anantvmanoj@gmail.com>
2025-11-09 13:30:22 +0530

    Topic Selection UI

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import SelectTopic from './_components/SelectTopic'
 
 function CreateNew() {
+  const [formData, setFormData]=useState([]);
+  const onHandleInputChange=(fieldName,fieldValue)=>{
+    console.log(fieldName, fieldValue)
+  }
+
   return (
-    <div>CreateNew</div>
+    <div className='md:px-20'>
+      <h2 className='font-bold text-4xl text-primary text-center'>Create New</h2>
+
+      <div className='mt-10 shadow-md mt-10 p-10'>
+        {/* Select Topic */}
+        <SelectTopic onUserSelect={onHandleInputChange}/>
+        {/* Select Style */}
+
+        {/* Duration */}
+
+        {/* Create Button */}
+      </div>
+    </div>
   )
 }
 
